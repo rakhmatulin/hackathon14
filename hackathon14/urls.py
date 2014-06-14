42344921381f190a5856a50e2d90f4cccb834cf1
Igor Nemilentsev <trezorg@gmail.com>
2014-06-14 13:42:47 +0300

    Static, media files

--- hackathon14/urls.py
+++ hackathon14/urls.py
@@ -1,11 +1,18 @@
 from django.conf.urls import patterns, include, url
-
+from django.conf import settings
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
     url(r'^$', 'hackathon14.views.index', name='index'),
-
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    if settings.MEDIA_ROOT:
+        urlpatterns += static(
+            settings.MEDIA_URL,
+            document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()

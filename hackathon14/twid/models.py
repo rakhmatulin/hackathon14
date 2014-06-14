619637e6f64bc81d55cf389bb7f61d1a8fc7e10b
Alexander Chaika <manti.by@gmail.com>
2014-06-14 14:20:14 +0300

    Add device model

--- hackathon14/twid/models.py
+++ hackathon14/twid/models.py
@@ -15,3 +15,12 @@ class Employer(models.Model):
     #image = models.ImageField()
     room = models.PositiveIntegerField(default=0)
     email = models.EmailField()
+
+
+class Device(models.Model):
+    sku = models.CharField(max_length=50, blank=True)
+    model = models.CharField(max_length=50, blank=True)
+    os = models.CharField(max_length=50, blank=True)
+    version = models.CharField(max_length=50, blank=True)
+    mac = models.CharField(max_length=50, blank=True)
+    image = models.ImageField(max_length=255, null=True, blank=True, upload_to='device')

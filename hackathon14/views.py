0c5b0180b9e94f435012ddb64c1d5e4b8be2868f
Igor Nemilentsev <trezorg@gmail.com>
2014-06-15 02:25:23 +0300

    updated history page

--- hackathon14/views.py
+++ hackathon14/views.py
@@ -27,17 +27,17 @@ def index(request):
         3: 'employer_id',
     }
     order_param = order_params[device_filter]
-    devices = Device.objects.select_related('employer')
+    devices_query = Device.objects.select_related('employer')
     if device_filter in (1, 3):
         order_extra_param = order_extra_params[device_filter]
-        devices = devices.extra(select={
+        devices_query = devices_query.extra(select={
             'null_values': 'CASE WHEN %s.%s IS NULL THEN 0 ELSE 1 END' %
             (Device._meta.db_table, order_extra_param)}
         ).order_by('-null_values', order_param)
     else:
-        devices = devices.order_by(order_param)
+        devices_query = devices_query.order_by(order_param)
     page = data.get('page')
-    paginator = Paginator(devices, PAGINATOR_COUNT)
+    paginator = Paginator(devices_query, PAGINATOR_COUNT)
     try:
         devices = paginator.page(page)
     except PageNotAnInteger:
@@ -79,17 +79,17 @@ def employer_list(request):
         3: '-count',
     }
     order_param = order_params[employer_filter]
-    employers = Employer.objects.annotate(count=Count('device'))
+    employers_query = Employer.objects.annotate(count=Count('device'))
     if employer_filter == 1:
         order_extra_param = 'date_action'
-        employers = employers.extra(select={
+        employers_query = employers_query.extra(select={
             'null_values': 'CASE WHEN %s.%s IS NULL THEN 0 ELSE 1 END' %
             (Employer._meta.db_table, order_extra_param)}
         ).order_by('-null_values', order_param)
     else:
-        employers = employers.order_by(order_param)
+        employers_query = employers_query.order_by(order_param)
     page = data.get('page')
-    paginator = Paginator(employers, PAGINATOR_COUNT)
+    paginator = Paginator(employers_query, PAGINATOR_COUNT)
     try:
         employers = paginator.page(page)
     except PageNotAnInteger:
@@ -101,12 +101,17 @@ def employer_list(request):
     return render_to_response('employer_list.html', locals())
 
 
-def history_list(request):
+def history_list(request, employer_id=None, device_id=None):
     data = request.GET.copy()
-    all_history = History.objects.select_related(
+    history_query = History.objects.select_related(
         'employer', 'device').order_by('-date')
+    if employer_id is not None:
+        history_query = history_query.filter(
+            Q(employer_id=employer_id) | Q(old_employer_id=employer_id))
+    if device_id is not None:
+        history_query = history_query.filter(device_id=device_id)
     page = data.get('page')
-    paginator = Paginator(all_history, PAGINATOR_COUNT)
+    paginator = Paginator(history_query, PAGINATOR_COUNT)
     try:
         history = paginator.page(page)
     except PageNotAnInteger:

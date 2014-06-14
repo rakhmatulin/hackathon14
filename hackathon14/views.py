5a0ff2a2fdeae34d844f907ff16bd1866bc91d24
Igor Nemilentsev <trezorg@gmail.com>
2014-06-14 23:01:04 +0300

    Fixed models dates

--- hackathon14/views.py
+++ hackathon14/views.py
@@ -1,22 +1,25 @@
+from django.db.models import Count
 from django.shortcuts import render_to_response
-from django.core.paginator import Paginator
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from hackathon14.twid.forms import IndexFilterForm
+from hackathon14.twid.forms import DevicesFilterForm, EmployersFilterForm
 from twid.models import Device, Employer
 
 
+EMPLOYER_PAGINATOR_COUNT = 30
+
+
 def index(request):
-    device_filter = IndexFilterForm.FILTER_CHOICE[0][0]
-    form = IndexFilterForm(request.GET or {'device_filter': device_filter})
+    device_filter = DevicesFilterForm.FILTER_CHOICE[0][0]
+    form = DevicesFilterForm(request.GET or {'device_filter': device_filter})
     if form.is_valid():
         device_filter = form.cleaned_data['device_filter']
     order_params = {
-        1: '-history__date',
+        1: '-date_action',
         2: 'model',
         3: 'employer__last_name_eng',
     }
-    devices = Device.objects.select_related('employer').\
-        order_by(order_params[device_filter])
+    order_param = order_params[device_filter]
+    devices = Device.objects.select_related('employer').order_by(order_param)
     devices_by_room = Device.get_devices_by_room()
     return render_to_response('index.html', locals())
 
@@ -26,11 +29,20 @@ def about(request):
 
 
 def employer_list(request):
-    all_employers = Employer.objects.all()
-
+    device_filter = EmployersFilterForm.FILTER_CHOICE[0][0]
+    form = EmployersFilterForm(request.GET or {'device_filter': device_filter})
+    if form.is_valid():
+        device_filter = form.cleaned_data['device_filter']
+    order_params = {
+        1: '-date_action',
+        2: 'last_name_eng',
+        3: '-count',
+    }
+    order_param = order_params[device_filter]
+    all_employers = Employer.objects.\
+        annotate(count=Count('device')).order_by(order_param)
     page = request.GET.get('page')
-    paginator = Paginator(all_employers, 30)
-
+    paginator = Paginator(all_employers, EMPLOYER_PAGINATOR_COUNT)
     try:
         employers = paginator.page(page)
     except PageNotAnInteger:
@@ -39,5 +51,4 @@ def employer_list(request):
     except EmptyPage:
         # If page is out of range (e.g. 9999), deliver last page of results.
         employers = paginator.page(paginator.num_pages)
-
     return render_to_response('employer_list.html', locals())

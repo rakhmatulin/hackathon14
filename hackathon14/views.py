49b23583c15210987d4b34b2b816b28f8c9395c4
Igor Nemilentsev <trezorg@gmail.com>
2014-06-14 23:28:11 +0300

    Fixed pagination

--- hackathon14/views.py
+++ hackathon14/views.py
@@ -11,7 +11,10 @@ EMPLOYER_PAGINATOR_COUNT = 30
 
 def index(request):
     device_filter = DevicesFilterForm.FILTER_CHOICE[0][0]
-    form = DevicesFilterForm(request.GET or {'device_filter': device_filter})
+    data = request.GET.copy()
+    if not 'device_filter' in data:
+        data['device_filter'] = device_filter
+    form = DevicesFilterForm(data)
     if form.is_valid():
         device_filter = form.cleaned_data['device_filter']
     order_params = {
@@ -24,23 +27,28 @@ def index(request):
     devices_by_room = Device.get_devices_by_room()
     return render_to_response('index.html', locals())
 
+
 def search(request):
     query = request.GET.get('query')
     devices = Device.objects.select_related('employer').\
         filter(
             Q(sku__icontains=query) |
             Q(model__icontains=query)).\
-        order_by('-history__date')
+        order_by('-date_action')
     devices_by_room = Device.get_devices_by_room()
     return render_to_response('search.html', locals())
+
 
 def about(request):
     return render_to_response('about.html')
 
 
 def employer_list(request):
-    device_filter = EmployersFilterForm.FILTER_CHOICE[0][0]
-    form = EmployersFilterForm(request.GET or {'device_filter': device_filter})
+    employer_filter = EmployersFilterForm.FILTER_CHOICE[0][0]
+    data = request.GET.copy()
+    if not 'device_filter' in data:
+        data['device_filter'] = employer_filter
+    form = EmployersFilterForm(data)
     if form.is_valid():
         device_filter = form.cleaned_data['device_filter']
     order_params = {
@@ -48,10 +56,10 @@ def employer_list(request):
         2: 'last_name_eng',
         3: '-count',
     }
-    order_param = order_params[device_filter]
+    order_param = order_params[employer_filter]
     all_employers = Employer.objects.\
         annotate(count=Count('device')).order_by(order_param)
-    page = request.GET.get('page')
+    page = data.get('page')
     paginator = Paginator(all_employers, EMPLOYER_PAGINATOR_COUNT)
     try:
         employers = paginator.page(page)

5581e17a3a1946b0a52cf5bb5e176a761df1ff7e
Rahmatulin Ernest <rahmatulin@rahmatulin-K52JU.(none)>
2014-06-14 13:59:29 +0300

    Added draft single device view

--- hackathon14/urls.py
+++ hackathon14/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from hackathon14.twid.views import device_single
+import hackathon14
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'hackathon14.views.index', name='index'),
-
+    url(r'^device/(?P<device_id>\d+)/$', device_single, name='device_view'),
     url(r'^admin/', include(admin.site.urls)),
 )

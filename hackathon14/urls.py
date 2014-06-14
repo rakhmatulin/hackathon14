5b0768ebea7f77c62913a658fb1bbc4c3ec07f44
Igor Nemilentsev <trezorg@gmail.com>
2014-06-14 17:22:37 +0300

    Added methods for assign device

--- hackathon14/urls.py
+++ hackathon14/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from hackathon14.twid.models import Employer, History
-from hackathon14.twid.views import device_single
+from hackathon14.twid.views import device_single, assign_device
 
 admin.site.register(Employer)
 admin.site.register(History)
@@ -13,6 +13,8 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', 'hackathon14.views.index', name='index'),
     url(r'^device/(?P<device_id>\d+)/$', device_single, name='device_view'),
+    url(r'^device/assign/(?P<device_id>\d+)/$',
+        assign_device, name='assign_device'),
     url(r'^admin/', include(admin.site.urls)),
 )
 

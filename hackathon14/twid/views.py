758cbe7657f326b767bf7f3b178186e4c559c026
Rakhmatulin <rahmatulin@rahmatulin-K52JU.(none)>
2014-06-14 17:38:44 +0300

    Added history to single device page

--- hackathon14/twid/views.py
+++ hackathon14/twid/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render_to_response
-from hackathon14.twid.models import Device, Employer
+from hackathon14.twid.models import Device, Employer, History
 
 
 def device_single(request, device_id):
     device = Device.objects.get(id=device_id)
+    history = History.objects.filter(device=device)
+
     context = {
-        'device': device
+        'device': device,
+        'history': history,
     }
     return render_to_response('twid/device_single.html', context)
 

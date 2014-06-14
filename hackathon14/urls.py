77e878e755db1bf1ea19b366ff1b24059f700731
Alexander Chaika <manti.by@gmail.com>
2014-06-14 17:46:45 +0300

    Update pages

--- hackathon14/urls.py
+++ hackathon14/urls.py
@@ -2,14 +2,10 @@ from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.contrib import admin
 
-import hackathon14
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from hackathon14.twid.models import Employer, History
-from hackathon14.twid.views import device_single, assign_device, employer_single
+from twid.views import device_single, assign_device, employer_single
 
-admin.site.register(Employer)
-admin.site.register(History)
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -17,7 +13,7 @@ urlpatterns = patterns('',
     url(r'^employers/', 'hackathon14.views.employer_list', name='employer_list'),
     url(r'^about/', 'hackathon14.views.about', name='about'),
     url(r'^device/(?P<device_id>\d+)/$', device_single, name='single_device_view'),
-    url(r'^employe/(?P<employe_id>\d+)/$', employe_single, name='single_employe_view'),
+    url(r'^employer/(?P<employe_id>\d+)/$', employer_single, name='single_employe_view'),
     url(r'^device/assign/(?P<device_id>\d+)/$',
         assign_device, name='assign_device'),
     url(r'^admin/', include(admin.site.urls)),

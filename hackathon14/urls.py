d389e012ab451d58eddada362ef410549442cfe7
Alexander Chaika <manti.by@gmail.com>
2014-06-14 13:18:55 +0300

    Add homepage (bootstrap)

--- hackathon14/urls.py
+++ hackathon14/urls.py
@@ -5,8 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'hackathon14.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', 'hackathon14.views.index', name='index'),
 
     url(r'^admin/', include(admin.site.urls)),
 )

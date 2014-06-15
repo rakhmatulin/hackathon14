58103beb417a43b501fe152859aed9641903f9b3
Rakhmatulin <rahmatulin@rahmatulin-K52JU.(none)>
2014-06-15 04:22:50 +0300

    Added father

--- hackathon14/twid/views.py
+++ hackathon14/twid/views.py
@@ -1,6 +1,6 @@
 
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
 from datetime import datetime
